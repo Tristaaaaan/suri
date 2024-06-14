8d500e5c3b0f6f405c6223a73f1a0ade787792ac
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-06-14 13:29:20 +0800

    dev enviroment changes

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:suri/app.dart';
+import 'package:suri/firebase/prod/firebase_options.dart';
 
-void main() {
+@pragma('vm:entry-point')
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: "suri",
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await Firebase.initializeApp();
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
   runApp(const MainApp());
 }
 
