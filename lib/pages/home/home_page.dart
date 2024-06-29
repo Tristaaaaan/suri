6638d341d46d8687ce358753bfc01acde528a458
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-06-29 11:32:06 +0800

    added name formatter

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:suri/functions/name_formatter.dart';
 import 'package:suri/pages/home/profile_page.dart';
 import 'package:suri/provider/auth/user_info_providers.dart';
 
@@ -13,37 +14,51 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Column(
-          children: [Text("Hi Tristan,"), Text("Welcome back")],
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            userInfoStream.when(
+              data: (data) {
+                return Text(
+                  "Hi ${formatName(data.name!)},",
+                  style: const TextStyle(fontSize: 16),
+                );
+              },
+              error: (error, stackTrace) => Container(),
+              loading: () => Container(),
+            ),
+            const Text("Welcome back", style: TextStyle(fontSize: 16))
+          ],
         ),
         automaticallyImplyLeading: false,
         actions: [
           userInfoStream.when(
-              data: (user) {
-                return GestureDetector(
-                  onTap: () {
-                    ref.read(userNotificationProvider.notifier).state =
-                        user.notification;
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProfilePage(
-                          imageUrl: user.imageUrl!,
-                          name: user.name!,
-                        ),
+            data: (user) {
+              return GestureDetector(
+                onTap: () {
+                  ref.read(userNotificationProvider.notifier).state =
+                      user.notification;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfilePage(
+                        imageUrl: user.imageUrl!,
+                        name: user.name!,
                       ),
-                    );
-                  },
-                  child: CircleAvatar(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    backgroundImage: NetworkImage(
-                      user.imageUrl!,
                     ),
+                  );
+                },
+                child: CircleAvatar(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  backgroundImage: NetworkImage(
+                    user.imageUrl!,
                   ),
-                );
-              },
-              error: (error, stackTrace) => Container(),
-              loading: () => Container())
+                ),
+              );
+            },
+            error: (error, stackTrace) => Container(),
+            loading: () => Container(),
+          )
         ],
       ),
     );
